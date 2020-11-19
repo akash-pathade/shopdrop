414e2af133f29a6ccfe8db6732ac711bcd9ece48
Akash Pathade <pathadeji@gmail.com>
2020-11-19 23:04:55 +0530

    Test Request Param

--- shopdrop/src/main/java/com/akash/shopdrop/controller/PageController.java
+++ shopdrop/src/main/java/com/akash/shopdrop/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.akash.shopdrop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,6 +12,17 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting","welcome to spring mvc");
+		return mv;
+	}
+	
+	@RequestMapping(value= {"/test/{greeting}"})
+	public ModelAndView test(@PathVariable("greeting") String greeting) {
+		ModelAndView mv = new ModelAndView("page");
+		
+		if (greeting== null) {
+			greeting="Hi there !";
+		}
+		mv.addObject("greeting",greeting);
 		return mv;
 	}
 
