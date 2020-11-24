232c66e9c1a139ab02e6ffee52bf5357d2ad413e
Akash Pathade <pathadeji@gmail.com>
2020-11-24 23:03:43 +0530

    Refractores the Category Class

--- shopdropbackend/src/main/java/com/akash/shopdropbackend/dto/Category.java
+++ shopdropbackend/src/main/java/com/akash/shopdropbackend/dto/Category.java
@@ -1,13 +1,28 @@
 package com.akash.shopdropbackend.dto;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Entity;
+
+@Entity
 public class Category {
 
 //	Private fields
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	
 	private String name;
+	
 	private String description;
+	
+	@Column(name="image_url")
 	private String imageURL;
+	
+	@Column(name="is_active")
 	private boolean active = true;
 	
 	
@@ -65,6 +80,12 @@ public class Category {
 
 	public void setActive(boolean active) {
 		this.active = active;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + ", description=" + description + ", imageURL=" + imageURL
+				+ ", active=" + active + "]";
 	}
 
 }
