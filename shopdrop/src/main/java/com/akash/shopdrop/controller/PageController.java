b947f1c8af6d772048742f28a7f12d903e2d23b8
Akash Pathade <pathadeji@gmail.com>
2020-12-01 16:12:02 +0530

    Added Single product Page view and Handlings

--- shopdrop/src/main/java/com/akash/shopdrop/controller/PageController.java
+++ shopdrop/src/main/java/com/akash/shopdrop/controller/PageController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.akash.shopdropbackend.dao.CategoryDAO;
+import com.akash.shopdropbackend.dao.ProductDAO;
 import com.akash.shopdropbackend.dto.Category;
+import com.akash.shopdropbackend.dto.Product;
 
 @Controller
 public class PageController {
 
 	@Autowired
 	CategoryDAO categoryDAO;
+
+	@Autowired
+	ProductDAO productDAO;
 
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
@@ -44,7 +49,7 @@ public class PageController {
 	@RequestMapping(value = { "/show/all/products" })
 	public ModelAndView showAllProducts() {
 		ModelAndView mv = new ModelAndView("page");
-		//To be coded
+		// To be coded
 		mv.addObject("categories", categoryDAO.listCategories());
 		mv.addObject("pageTitle", "All Products");
 		mv.addObject("userClickAllProducts", true);
@@ -55,17 +60,34 @@ public class PageController {
 	@RequestMapping(value = { "/show/category/{id}/products" })
 	public ModelAndView showCatgeoryProducts(@PathVariable("id") int id) {
 		ModelAndView mv = new ModelAndView("page");
-		
-		//categoryDAO to fetch a single category
+
+		// categoryDAO to fetch a single category
 		Category category = categoryDAO.get(id);
-		
+
 		mv.addObject("categories", categoryDAO.listCategories());
-		
-		//Passing single Category Object
+
+		// Passing single Category Object
 		mv.addObject("category", category);
-		
+
 		mv.addObject("pageTitle", category.getName());
 		mv.addObject("userClickCategoryProducts", true);
+		return mv;
+	}
+
+	// Request Mapping for showing single product
+	@RequestMapping(value = { "/show/{id}/product" })
+	public ModelAndView showSingleProduct(@PathVariable("id") int id) {
+		ModelAndView mv = new ModelAndView("page");
+
+//		Increasing view of products
+		Product product = productDAO.get(id);
+		product.setViews(product.getViews() + 1);
+		productDAO.update(product);
+
+//		passing product object
+		mv.addObject("product", product);
+		mv.addObject("pageTitle", product.getName());
+		mv.addObject("userClickSingleProduct", true);
 		return mv;
 	}
 
---
+++ shopdrop/src/main/webapp/WEB-INF/views/singleProduct.jsp
@@ -0,0 +1,48 @@
+<div class="container">
+	<!-- This row is for breadcrumb -->
+	<div class="row">
+		<div class="col-xs-12">
+			<ol class="breadcrumb">
+				<li><a href="${contextRoot}/home"> Home </a></li>
+				<li><a href="${contextRoot}/show/all/products"> /Products </a></li>
+				<li class="active"><a
+					href="${contextRoot}/show/${product.id}/product">/
+						${product.name}</a></li>
+			</ol>
+		</div>
+	</div>
+
+	<!-- This row is to show product details -->
+	<div class="row">
+
+		<!-- For product Image -->
+		<div class="col-xs-12 col-md-4">
+
+			<img src="${images}/${product.code}.jpg" class="img img-responsive singleProdImg" />
+			<hr />
+		</div>
+
+		<!-- For remaining product description -->
+		<div class="col-xs-12 col-md-8">
+			<h3>${product.name}</h3>
+			<hr />
+
+			<p>${product.description}</p>
+			<hr />
+
+			<h4>
+				Price: <strong> &#8377; ${product.unitPrice} /- </strong>
+			</h4>
+			<hr />
+
+			<h6>Hurry up only ${product.quantity} items left</h6>
+
+			<a href="${contextRoot}/cart/add/${product.id}/product"
+				class="btn btn-success">Add to cart</a> <a
+				href="${contextRoot}/show/all/products" class="btn btn-info">Back</a>
+
+		</div>
+
+	</div>
+
+</div>
